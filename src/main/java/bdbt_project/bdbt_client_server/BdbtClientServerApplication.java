d15e3ef620ec235a2e4fbc43bb0b2a248c6f26e8
Adam Piwoński <88802068+Madapiw@users.noreply.github.com>
2022-05-05 16:41:33 +0100

    init

---
+++ src/main/java/bdbt_project/bdbt_client_server/BdbtClientServerApplication.java
@@ -0,0 +1,13 @@
+package bdbt_project.bdbt_client_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdbtClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdbtClientServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bdbt_project/bdbt_client_server/BdbtClientServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bdbt_project.bdbt_client_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdbtClientServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
